884e50f81d3b4702680896da295b1c80148a5ca1
Patrick <patrick.a.mclennan@gmail.com>
2018-11-05 03:50:15 -0500

    workin'

--- main.js
+++ main.js
@@ -1,15 +1,13 @@
 'use strict';
 
-// 'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=8baaf90261984e748f990e495360e903';
-
 const DOM = {
   slider: document.querySelector('.slider'),
   main: document.querySelector('.main'),
+  pic: document.querySelector('.main__pic'),
 };
 
 // NEWS
 class ApiCall {
-
   constructor(choice) {
     this.apiKey = 'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=8baaf90261984e748f990e495360e903';
     this.pic = '';
@@ -26,15 +24,15 @@ class ApiCall {
     return json;
   }
 
-  async pullData(choice) {
+  async pullData() {
     const allData = await this.getJSON();
-    const chosen = allData.articles[choice];
+    const chosen = allData.articles[this.choice];
     this.pic = chosen.urlToImage;
     this.title = chosen.title;
     this.content = chosen.content;
     this.url = chosen.url;
     this.author = chosen.author;
-    console.log(this);
+    return this;
   }
 }
 
@@ -43,9 +41,11 @@ DOM.slider.addEventListener('click', () => {
   DOM.slider.classList.toggle('slide');
 });
 
-const newNews = () => {
-  const newTry = new ApiCall();
-  newTry.pullData(5);
-};
+async function newNews(choice) {
+  const src = new ApiCall(choice);
+  await src.pullData();
+  DOM.pic.style.backgroundImage = `url(${src.pic})`;
+  console.log(src.pic);
+}
 
-newNews();
+newNews(7);
