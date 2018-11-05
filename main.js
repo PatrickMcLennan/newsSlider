c50d8d35a1600b6ffab93d5b150fdcad3aea5ca1
Patrick <patrick.a.mclennan@gmail.com>
2018-11-05 02:58:28 -0500

    workin'

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 'use strict';
 
-// VARIABLES
 const DOM = {
   slider: document.querySelector('.slider'),
   main: document.querySelector('.main'),
@@ -10,7 +9,6 @@ const DOM = {
 class Source {
   constructor() {
     this.apiKey = 'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=8baaf90261984e748f990e495360e903';
-    this.json = this.getJSON();
   }
 
   async getJSON() {
@@ -18,12 +16,12 @@ class Source {
     const data = response.json();
     return data;
   }
-}
 
-async function news() {
-  const src = new Source();
-  const json = await src.json;
-  console.log(json.articles);
+  async findSrc(selection) {
+    const jsonObj = await this.getJSON();
+    const json = jsonObj.articles;
+    return json[selection];
+  }
 }
 
 // DOM MANIPULATION
@@ -32,4 +30,19 @@ DOM.slider.addEventListener('click', () => {
 });
 
 // EVENTS
-document.onload = news();
+
+const news = () => {
+  const newTry = new Source();
+  console.log(newTry.findSrc(11));
+};
+
+news();
+
+// CNBC, [1]
+// Forbes, [4]
+// MArketWatch, [5]
+// Reuters, [7]
+// Gizmodo, [8]
+// WSJ, [10]
+// NYT, [11]
+// CNN [13]
