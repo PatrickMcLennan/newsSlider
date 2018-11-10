8697603217241e01a3b0f2221068d9a9e4b04f8e
Patrick <patrick.a.mclennan@gmail.com>
2018-11-10 16:38:05 -0500

    workin away

--- src/index.js
+++ src/index.js
@@ -1,9 +1,3 @@
 import NEWS from './news';
-import Article from './article';
 import DOM from './dom';
-
-(() => {
-  console.log(NEWS);
-  console.log(Article);
-  console.log(DOM);
-})();
+import Article from './article';
