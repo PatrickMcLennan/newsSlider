db80fc4213cefdd1a348481433f7d34f61b60e74
Patrick <patrick.a.mclennan@gmail.com>
2018-11-11 15:15:14 -0500

    workin away

--- src/article.js
+++ src/article.js
@@ -25,18 +25,15 @@ class Article {
       </div>
 
       <div class="article__info">
-
         <div class="article__info__content">
         <p>
         ${this.content}
         </p>
         </div>
-
         <div class="article__info__links">
           <h5 class="article__info__author"><span>Written By :</span> ${this.author}</h5>
           <h5 class="article__info__date">${this.date}</h5>
         </div>
-
       </div>
     `;
     return newArticle;
--- src/index.js
+++ src/index.js
@@ -3,19 +3,18 @@ import NEWS from './news';
 import Article from './article';
 
 function createHTML() {
-  NEWS.arrayPosition += 1;
-  const article = new Article(NEWS.arrayPosition);
+  let { arrayPosition: story } = NEWS;
+  story += 1;
+  const article = new Article(story);
   const html = article.pic === null ? createHTML() : article.createArticle();
   return html;
 }
 
 function placeHTML() {
-  const { slider, main, currentArticle } = DOM;
+  const { slider } = DOM;
   const newArticle = createHTML();
-  main.appendChild(newArticle);
   const placedArticle = slider.classList.contains('slide') ? newArticle.classList.add('placeRight') : newArticle.classList.add('placeLeft');
   return placedArticle;
-
 }
 
 function animate() {
