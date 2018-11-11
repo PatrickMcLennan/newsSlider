812553ba0926456e9d2cdbf44e68416528f0cfd1
Patrick <patrick.a.mclennan@gmail.com>
2018-11-11 14:23:58 -0500

    workin away

--- src/index.js
+++ src/index.js
@@ -2,30 +2,33 @@ import DOM from './dom';
 import NEWS from './news';
 import Article from './article';
 
-function create() {
+function createHTML() {
   NEWS.arrayPosition += 1;
   const article = new Article(NEWS.arrayPosition);
-  const html = article.pic === null ? create() : article.createArticle();
+  const html = article.pic === null ? createHTML() : article.createArticle();
   return html;
 }
 
-function getDirection(newArt, currentArt, slider) {
-  if (slider.classList.contains('slide')) {
-    currentArt.style.transform = 'translateX(-100vw)';
-    newArt.style.transform = 'translateX(-100vw)';
-  } else {
-    currentArt.style.transform = 'translateX(100vw)';
-    newArt.style.transform = 'translateX(100vw)';
+const sliderRight = () => {
+  let slidRight = false;
+  if (DOM.slider.classList.contains('slide')) {
+    slidRight = true;
   }
+  return slidRight;
+};
+
+function placeHTML() {
+  const { main, slider } = DOM;
+  const newArticle = createHTML();
+  main.appendChild(newArticle);
+  sliderRight() === false ? newArticle.classList.add('placeLeft') : newArticle.classList.add('placeRight');
+  return newArticle;
 }
 
 function animate() {
-  const newArticle = create();
   const { main, slider } = DOM;
   const currentArticle = main.getElementsByTagName('section');
-  getDirection(newArticle, currentArticle, slider);
-  slider.classList.toggle('slide');
-  document.removeChild(currentArticle);
+  const newArticle = placeHTML();
 }
 
-DOM.slider.addEventListener('click', animate);
+DOM.slider.addEventListener('click', placeHTML);
